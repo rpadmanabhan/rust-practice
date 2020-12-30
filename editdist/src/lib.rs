b807721b27c7df595ab1a0df79375a430d9b6ddf
rpadmanabhan <raghus606@gmail.com>
2020-12-30 00:20:07 +0000

    Added logic to create cigar string

--- editdist/src/lib.rs
+++ editdist/src/lib.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::convert::TryFrom;
 
 #[derive(Debug)]
@@ -7,27 +6,69 @@ pub struct AlignResult {
     pub cigar: String,
 }
 
-// TO DO: Return cigar string from alignment
-impl AlignResult {
-    pub fn new(
-        edit_dist: u32,
-        cigar_ops: Vec<u8>,
-        cigar_ops_counts: Vec<u32>,
-    ) -> Result<AlignResult, &'static str> {
-        let cigar_ops_to_char: HashMap<u8, char> =
-            [(0, 'M'), (1, 'I'), (2, 'D')].iter().cloned().collect();
-
-        let mut cigar = String::with_capacity(1 * cigar_ops.len() + 4 * cigar_ops_counts.len());
-        for (i, c) in cigar_ops.iter().enumerate() {
-            cigar.push_str(&cigar_ops_counts[i].to_string());
-            cigar.push(cigar_ops_to_char[c]);
-        }
-
-        Ok(AlignResult { edit_dist, cigar })
+// Compare 2 AlignResults for equality
+impl PartialEq for AlignResult {
+    fn eq(&self, other: &Self) -> bool {
+        self.edit_dist == other.edit_dist && self.cigar == other.cigar
     }
 }
 
-pub fn edit_dist(s1: &str, s2: &str) -> Option<u32> {
+pub fn traceback(dp_mat: &Vec<Vec<u32>>) -> String {
+    let mut alignment = String::new();
+
+    let mut i: usize = dp_mat.len() - 1;
+    let mut j: usize = dp_mat[0].len() - 1;
+
+    // start from the bottom of the DP matrix and walk backwards
+    // making lowest cost moves in the walk back
+    while !(i == 0 && j == 0) {
+        // Edge conditions - reached top row/col
+        if i == 0 {
+            alignment.push_str("I");
+            j = j - 1;
+        } else if j == 0 {
+            alignment.push_str("D");
+            i = i - 1;
+        } else {
+            // not at the edges - make lowest cost moves
+            let mut move_choices = vec![
+                ("M", dp_mat[i - 1][j - 1]),
+                ("I", dp_mat[i - 1][j]),
+                ("D", dp_mat[i][j - 1]),
+            ];
+            move_choices.sort_by_key(|k| k.1);
+            alignment.push_str(move_choices[0].0);
+            if move_choices[0].0 == "M" {
+                i = i - 1;
+                j = j - 1;
+            } else if move_choices[0].0 == "I" {
+                i = i - 1;
+            } else if move_choices[0].0 == "D" {
+                j = j - 1;
+            }
+        }
+    }
+
+    // Reverse the alignment string and bin adjacent positions to create a cigar string
+    let mut cigar = String::new();
+    let mut prev_char = alignment.chars().rev().next().unwrap();
+    let mut count = 0;
+    for c in alignment.chars().rev() {
+        if prev_char == c {
+            count = count + 1;
+        } else {
+            cigar.push_str(format!("{}{}", count, prev_char).as_str());
+            count = 1;
+        }
+        prev_char = c;
+    }
+    cigar.push_str(format!("{}{}", count, prev_char).as_str());
+
+    cigar
+}
+
+// Calculate editdistance
+pub fn edit_dist(s1: &str, s2: &str) -> Option<AlignResult> {
     let s1_bytes = s1.as_bytes();
     let s2_bytes = s2.as_bytes();
 
@@ -43,18 +84,13 @@ pub fn edit_dist(s1: &str, s2: &str) -> Option<u32> {
         }
     }
 
-    // Track alignment cigar
-    let mut i: usize = 1;
-    let mut cigar_idx: usize = 0;
-    let mut cigar_ops: Vec<u8> = Vec::new();
-    let mut cigar_ops_counts: Vec<u32> = Vec::new();
-
     // traverse DP matrix - row wise
+    let mut i: usize = 1;
     while i < s1.len() + 1 {
         let mut j: usize = 1;
 
         while j < s2.len() + 1 {
-            // Match / Mismatch
+            // Match / Mismatch penalty
             let mut delta = 0;
             if s1_bytes[i - 1] != s2_bytes[j - 1] {
                 delta = 1;
@@ -71,22 +107,15 @@ pub fn edit_dist(s1: &str, s2: &str) -> Option<u32> {
             // Update DP Matrix
             dp_mat[i][j] = move_choices[0].1;
 
-            // Update Cigar
-            let cigar_op = move_choices[0].0;
-            if (cigar_idx > 0) && (cigar_op == cigar_ops[cigar_idx - 1]) {
-                cigar_ops_counts[cigar_idx - 1] += 1;
-            } else {
-                cigar_ops.push(cigar_op);
-                cigar_ops_counts.push(1);
-                cigar_idx += 1;
-            }
             j += 1;
         }
         i += 1;
     }
 
-    // Return Editdistance
-    Some(dp_mat[s1.len()][s2.len()])
+    Some(AlignResult {
+        edit_dist: dp_mat[s1.len()][s2.len()],
+        cigar: traceback(&dp_mat),
+    })
 }
 
 #[cfg(test)]
@@ -97,34 +126,69 @@ mod tests {
     fn equal_length() {
         let s1 = "ACGTA";
         let s2 = "GCGTA";
-        assert_eq!(std::option::Option::Some(1), edit_dist(s1, s2))
+        let align_result = edit_dist(s1, s2).unwrap();
+        assert_eq!(
+            align_result,
+            AlignResult {
+                edit_dist: 1,
+                cigar: String::from("5M")
+            }
+        );
     }
 
     #[test]
     fn diff_length() {
         let s1 = "ACGTAAACAC";
         let s2 = "ACGTAACAC";
-        assert_eq!(std::option::Option::Some(1), edit_dist(s1, s2))
+        let align_result = edit_dist(s1, s2).unwrap();
+        assert_eq!(
+            align_result,
+            AlignResult {
+                edit_dist: 1,
+                cigar: String::from("6M1I3M")
+            }
+        );
     }
 
     #[test]
     fn large_del() {
         let s1 = "ACGTAAAAACCCAGGGCACACGTGGGGCACACACA";
         let s2 = "ACGTCACACGTGGGGCACACACA";
-        assert_eq!(std::option::Option::Some(12), edit_dist(s1, s2))
+        let align_result = edit_dist(s1, s2).unwrap();
+        assert_eq!(
+            align_result,
+            AlignResult {
+                edit_dist: 12,
+                cigar: String::from("4M5I1M2I1M3I3M2I14M")
+            }
+        );
     }
 
     #[test]
     fn large_ins() {
         let s1 = "ACGTCACACGTGGGGCACACACAGGGGTTGTGTG";
         let s2 = "ACGTCACACGTGGGGCACACACA";
-        assert_eq!(std::option::Option::Some(11), edit_dist(s1, s2))
+        let align_result = edit_dist(s1, s2).unwrap();
+        assert_eq!(
+            align_result,
+            AlignResult {
+                edit_dist: 11,
+                cigar: String::from("23M11I")
+            }
+        );
     }
 
     #[test]
     fn ins_del_mismatch() {
         let s1 = "ACGTCACACGTGGGGCACACACAGGGGTTGTGTG";
         let s2 = "ATGTCACACGTGGGGCACACA";
-        assert_eq!(std::option::Option::Some(14), edit_dist(s1, s2))
+        let align_result = edit_dist(s1, s2).unwrap();
+        assert_eq!(
+            align_result,
+            AlignResult {
+                edit_dist: 14,
+                cigar: String::from("21M13I")
+            }
+        );
     }
 }
